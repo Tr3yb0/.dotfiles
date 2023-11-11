9caa5455c6d5e31e8011c23afd2e9c9819924244
Tr3yb0 <trey.botard@gmail.com>
2023-11-11 11:03:37 -0500

    update gitignore, skhdrc, install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew bundle
+brew bundle --file=~/.dotfiles/Brewfile
 
 ln -sv ~/.dotfiles/.oh-my-zsh/ ~
 ln -sv ~/.dotfiles/karabiner ~
 ln -sv ~/.dotfiles/iterm2 ~
 ln -sv ~/.dotfiles/gh ~
-ln -sv ~/.dotfiles/yabai/yabairc ~
+ln -sv ~/.dotfiles/yabai/yabairc ~/.yabairc
+ln -sv ~/.dotfiles/nvim ~/.config/nvim
+
 
 
